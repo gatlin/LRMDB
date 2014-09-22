bac37db1eb3c4998a6aab86ec37b125cad054472
Gatlin C Johnson <gatlin@niltag.net>
2014-09-21 21:19:04 -0500

    server component

---
+++ server/server/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from rest_framework import routers
+from maint import views
+
+router = routers.SimpleRouter()
+router.register(r'issues', views.IssueViewSet)
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'server.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^api/v1/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls',
+        namespace='rest_framework')),
+    url(r'^admin/', include(admin.site.urls)),
+    
+)
